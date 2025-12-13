2f37d11eed1c8e437d8932fbf5a936d3b2a13777
zNitche <53448802+zNitche@users.noreply.github.com>
2025-12-13 17:13:38 +0100

    feat: basic listing available cli commands + args

--- rubix_cli/cli.py
+++ rubix_cli/cli.py
@@ -1,5 +1,43 @@
 import argparse
+import inspect
 from rubix_cli.core import Commander
+
+
+def __get_func_args(func):
+    args = {}
+    inspect_data = inspect.signature(func).parameters
+
+    for key, val in inspect_data.items():
+        args[key] = val.annotation.__name__
+
+    return args
+
+
+def get_commands(commander: Commander):
+    commands = {
+        "ls": {
+            "description": "",
+            "func": commander.list_files,
+            "args": __get_func_args(commander.list_files)
+        }
+    }
+
+    return commands
+
+
+def list_commands(commander: Commander):
+    commands = get_commands(commander)
+
+    for command_name in commands:
+        cmd_details = commands[command_name]
+
+        print(f"### {command_name}")
+        print(f"args: {cmd_details['args']}")
+
+        description = cmd_details.get("description")
+
+        if description:
+            print(f"description: {description}")
 
 
 def main(args: argparse.Namespace):
@@ -7,7 +45,10 @@ def main(args: argparse.Namespace):
     debug = args.debug
 
     commander = Commander(interface=interface, debug=debug)
-    files = commander.list_files()
+
+    list_commands(commander)
+
+    files = commander.list_files(path="/")
 
     print(files)
 
