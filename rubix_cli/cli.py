47d68abb0cf5eb2680d906b84e9bc1dfab38059d
zNitche <53448802+zNitche@users.noreply.github.com>
2025-12-13 16:52:30 +0100

    feat: cli entrypoint

---
+++ rubix_cli/cli.py
@@ -0,0 +1,30 @@
+import argparse
+from rubix_cli.core import Commander
+
+
+def main(args: argparse.Namespace):
+    interface = args.interface
+    debug = args.debug
+
+    commander = Commander(interface=interface)
+    files = commander.list_files()
+
+    print(files)
+
+
+def get_args():
+    argument_parser = argparse.ArgumentParser()
+
+    argument_parser.add_argument(
+        "--interface", type=str, help="example, /dev/tty1", required=True)
+
+    argument_parser.add_argument("--debug", action=argparse.BooleanOptionalAction,
+                                 default=False, help="debug mode", required=False)
+
+    return argument_parser.parse_args()
+
+
+def run():
+    args = get_args()
+
+    main(args)
