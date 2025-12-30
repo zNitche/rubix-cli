fd0fa646bc35c6a567a1189053365c9742d3eff1
zNitche <53448802+zNitche@users.noreply.github.com>
2025-12-30 20:10:29 +0100

    refactor: better FlashCommand structure

--- rubix_cli/core/commands/flash_command.py
+++ rubix_cli/core/commands/flash_command.py
@@ -4,6 +4,31 @@ from rubix_cli.snippets import filesystem_snippets, flash_snippets
 
 
 class FlashCommand(CommandBase):
+    def __purge(self):
+        self._commander._logger.info("purging...")
+
+        cmd = filesystem_snippets.SnippetPurge().get_code()
+        self._commander.default_cmd_handler(cmd)
+
+        self._commander.soft_reboot()
+
+    def __flash_object(self, flash_snippet: flash_snippets.SnippetFlash,
+                       filename: str = "", file_content: bytes = b"",
+                       path: str = "", dirname: str = ""):
+
+        if file_content:
+            self._commander._logger.info(message=f"flashing {path}...")
+        else:
+            self._commander._logger.info(
+                message=f"creating {path} directory...")
+
+        cmd = flash_snippet.get_code(
+            {"filename": filename, "dirname": dirname,
+             "file_content": file_content, "path": path})
+
+        self._commander.default_cmd_handler(
+            cmd, reboot=False, raise_exception_on_errors=True)
+
     def exec(self, root_path: str = ""):
         if not root_path:
             raise Exception("path can't be empty")
@@ -13,12 +38,7 @@ class FlashCommand(CommandBase):
         if os.path.isfile(root_path):
             raise Exception(f"{root_path} should point to directory")
 
-        self._commander._logger.info("purging...")
-
-        cmd = filesystem_snippets.SnippetPurge().get_code()
-        self._commander.default_cmd_handler(cmd)
-
-        self._commander.soft_reboot()
+        self.__purge()
 
         flash_snippet = flash_snippets.SnippetFlash()
         self._commander._logger.info(message="purged, flashing...")
@@ -28,23 +48,15 @@ class FlashCommand(CommandBase):
 
             for dirname in dirnames:
                 path = f"{root}/{dirname}"
-                self._commander._logger.info(
-                    message=f"creating {path} directory...")
 
-                cmd = flash_snippet.get_code(
-                    {"dirname": dirname, "path": path})
-                self._commander.default_cmd_handler(
-                    cmd, reboot=False, raise_exception_on_errors=True)
+                self.__flash_object(
+                    flash_snippet, dirname=dirname, path=path)
 
             for filename in filenames:
                 with open(os.path.join(dirpath, filename), "rb") as file:
                     content = file.read()
 
                 path = f"{root}/{filename}"
-                self._commander._logger.info(message=f"flashing {path}...")
 
-                cmd = flash_snippet.get_code(
-                    {"filename": filename, "file_content": content, "path": path})
-
-                self._commander.default_cmd_handler(
-                    cmd, reboot=False, raise_exception_on_errors=True)
+                self.__flash_object(
+                    flash_snippet, filename=filename, file_content=content, path=path)
